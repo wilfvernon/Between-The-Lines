7db0066dca07694d4aa91edefbc582f0f1ac355a
wilfvernon <50486710+wilfvernon@users.noreply.github.com>
2026-03-08 00:53:47 +0000

    Use crest image for all PWA install icons

--- app/vite.config.js
+++ app/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      includeAssets: ['apple-touch-icon.png', 'pwa-192x192.png', 'pwa-512x512.png'],
       manifest: {
         name: 'Between the Lines',
         short_name: 'BTL',
